cb1e636501b3520ff9a257ffd9ffcc933bb8889c
noe10 <escorpio.rosa.94@gmail.com>
2019-11-17 23:29:28 -0500

    created backend

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-console.log("asdasd");
+
+// Register module/require aliases
+require('module-alias/register');
+
+// Load .env Enviroment Variables to process.env
+require('mandatoryenv').load([
+    'DB_URL',
+    'DB_COLLECTION',
+    'PORT'
+]);
+
+// Require dependencies
+const http = require('http');
+const socketio = require('socket.io');
+
+const { PORT } = process.env;
+
+const server = http.createServer();
+const io = socketio(server);
+
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
