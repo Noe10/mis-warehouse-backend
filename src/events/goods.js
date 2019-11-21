e8d40a910e7aaf8fc03c75eba24bfb7cd6e6a550
noe10 <escorpio.rosa.94@gmail.com>
2019-11-21 00:23:33 -0500

    deleted controllers and create de events/googs

--- src/events/goods.js
+++ src/events/goods.js
@@ -1,6 +1,34 @@
+const  modelGoods = require('../models/goods');
 module.exports = {
-    createGoods(socket,data){
-        console.log();
+    async createGoods(socket,data){
+       let resul = await modelGoods.create(data);
+    console.log("datos registrados correctamente", resul);
+    
     },
+    async getAll(){
+        let resul = await modelGoods.getAll();
+        console.log('Listando informacion', resul);
+        
+    },
+    async searchGoods( data){
+        let { id } = data;
+        let resul = await modelGoods.search(id);
+        if(resul.lenght > 0 ){
+            console.log('the information is ',{resul});
+        }else{
+            console.log('don´t exist infomation');
+            
+        }
+    },
+    async UpdateGoods(data){
+        let resul = await modelGoods.update(data);
+        console.log('datos actualizados correctamente', resul);
+        
+    }, 
+    async DeleteGoogs(data){
+        let resul = await modelGoods.delete(data);
+        console.log('datos eliminados correctamente', resul);
+        
+    }
 
 }
